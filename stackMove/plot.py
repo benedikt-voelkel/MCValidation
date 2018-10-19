51983559208ac6701ef751a3f127bc139af77265
bvolkel <benedikt.volkel@cern.ch>
2018-10-19 10:55:15 +0200

    Add error propagation to ratio plot

--- stackMove/plot.py
+++ stackMove/plot.py
@@ -18,13 +18,18 @@ Some global variables:
         YDATA_NAME:
 '''
 XDATA_NAME = "xData"
+XDATA_ERROR_NAME = "xDataError"
 YDATA_NAME = "yData"
+YDATA_ERROR_NAME = "yDataError"
 FILES = "files"
+# Available extraction algorithms are 'identity' and 'mean'
 EXTRACTION_ALGORITHM = "extractionAlgorithm"
 XAXIS_LABEL = "xAxisLabel"
 YAXIS_LABEL = "yAxisLabel"
 PLOT_LABEL = "label"
 INFILE_DIR = "indir"
+# Available plot types are 'hist' and 'scatter'
+PLOT_TYPE = "plotType"
 
 
 
@@ -82,7 +87,7 @@ def plot(plotDicts, outputPath, ratioIndex, xmax, xmin, ymax, ymin):
     axes[0].set_ylabel(plotDicts[0][YAXIS_LABEL])
 
     colors = iter(cm.rainbow(np.linspace(0, 1, len(plotDicts))))
-    markers = iter([".", "+", "^"])
+    markers = iter([".", "^", "v"])
     if xmax:
         axes[0].set_xlim(right=xmax)
     if xmin:
@@ -94,10 +99,31 @@ def plot(plotDicts, outputPath, ratioIndex, xmax, xmin, ymax, ymin):
     for pd in plotDicts:
         color = next(colors)
         marker = next(markers)
-        axes[0].plot(pd[XDATA_NAME], pd[YDATA_NAME], color=color, marker=marker, linestyle="", label=pd[PLOT_LABEL])
+        axes[0].errorbar(pd[XDATA_NAME], pd[YDATA_NAME], xerr=pd[XDATA_ERROR_NAME], yerr=pd[YDATA_ERROR_NAME], color=color, marker=marker, linestyle="", label=pd[PLOT_LABEL])
         if referenceDict and pd != referenceDict:
             ratio = np.array(pd[YDATA_NAME]) / np.array(referenceDict[YDATA_NAME])
-            axes[1].plot(pd[XDATA_NAME], ratio, color=color, marker=marker, linestyle="")
+            ratioNomError = None
+            ratioError = None
+
+            if pd[YDATA_ERROR_NAME]:
+                ratioNomError = np.square(np.array(pd[YDATA_ERROR_NAME]) / np.array(referenceDict[YDATA_NAME]))
+
+            if referenceDict[YDATA_ERROR_NAME]:
+                ratioError = np.square(np.array(pd[YDATA_NAME]) * np.array(referenceDict[YDATA_ERROR_NAME])  / np.square(np.array(referenceDict[YDATA_NAME])))
+
+
+            # Sum the squares if possible
+            if ratioNomError is not None and ratioError is not None:
+                ratioError = ratioError + ratioNomError
+            # Otherwise try only the nomError
+            elif ratioNomError is not None and ratioError is None:
+                ratioError = ratioNomError
+            if ratioError is not None:
+                ratioError = np.sqrt(ratioError)
+            # So now ratioError is either None or a filled numpy array
+
+            # Plot the stuff
+            axes[1].errorbar(pd[XDATA_NAME], ratio, yerr=ratioError, color=color, marker=marker, linestyle="")
 
     axes[0].legend(loc="best", fontsize=10)
     plt.savefig(outputPath + ".eps")
@@ -151,44 +177,49 @@ def extractFiles(configFile, indir):
     configDict[PLOT_LABEL] = configDict.get(PLOT_LABEL, "label")
 
     # Check for required x data
-    xData = configDict.get(XDATA_NAME, None)
-    if not xData:
+    if not configDict.get(XDATA_NAME, None):
         print "ERROR: No x-data found"
         exit(1)
-
+    configDict[XDATA_ERROR_NAME] = configDict.get(XDATA_ERROR_NAME, None)
+    # If number of error values is different from number of nominal values, fail
+    if configDict[XDATA_ERROR_NAME] and len(configDict[XDATA_NAME]) != len(configDict[XDATA_ERROR_NAME]):
+        print "ERROR: Same number of nominal values and errors required for x-data"
+        exit(1)
 
     # Check if y data are already there and just return if so.
-    yData = configDict.get(YDATA_NAME, None)
-    if yData:
-        if len(yData) != len(xData):
-            print "ERROR: Same number of x- and y-data required"
+    configDict[YDATA_ERROR_NAME] = configDict.get(YDATA_ERROR_NAME, None)
+    if configDict.get(YDATA_NAME, None):
+        # Check for same length of x and y
+        if len(configDict[YDATA_NAME]) != len(configDict[XDATA_NAME]):
+            print "ERROR: Same number of x- and y-data (including errors) required"
+            exit(1)
+        if configDict[YDATA_ERROR_NAME] and len(configDict[YDATA_ERROR_NAME]) != len(configDict[YDATA_NAME]):
+            print "ERROR: Same number for y-data and respective errors required"
             exit(1)
         return configDict
 
 
     # Expect input files to read data from.
-    inputFiles = configDict.get(FILES, None)
-    if not inputFiles:
+    if not configDict.get(FILES, None):
         print "ERROR: Input files or y-data required"
         exit(1)
 
-    if len(xData) != len(inputFiles):
+    if len(configDict[XDATA_NAME]) != len(configDict[FILES]):
         print "ERROR: Need as many x-data as files " + configFile
         exit(1)
 
-    # Check whether an extraction alorithm is given.
-    extractionAlgorithm = configDict.get(EXTRACTION_ALGORITHM, "identity")
-    if inputFiles and not extractionAlgorithm:
-        print "WARNING: Found inout files but no extraction algorithm specified. Assume one number per file"
+    # Get extraction algorithm for y data.
+    configDict[EXTRACTION_ALGORITHM] = configDict.get(EXTRACTION_ALGORITHM, "identity")
 
     # Joing file names with prefix
     prefix = configDict.get(INFILE_DIR, indir)
-    inputFiles = [os.path.join(prefix, inFile) for inFile in inputFiles]
-    yData = []
-    for file in inputFiles:
-        yData.append(extract(file, extractionAlgorithm))
-
-    configDict[YDATA_NAME] = yData
+    configDict[FILES] = [os.path.join(prefix, inFile) for inFile in configDict[FILES]]
+    configDict[YDATA_NAME] = []
+    configDict[YDATA_ERROR_NAME] = []
+    for file in configDict[FILES]:
+        value, error = extract(file, configDict[EXTRACTION_ALGORITHM])
+        configDict[YDATA_NAME].append(value)
+        configDict[YDATA_ERROR_NAME].append(error)
 
     return configDict
 
@@ -202,20 +233,24 @@ Description:
 '''
 def extract(filepath, algorithm):
 
-    returnValue = -1.
+    value = -1.
+    error = 0.
     with open(filepath, 'r') as f:
         if algorithm == "identity":
-            returnValue = extractIdentity(f)
+            value, error = extractIdentity(f)
         elif algorithm == "mean":
-            returnValue = extractMean(f)
+            value, error = extractMean(f)
         else:
             print "ERROR: Unknown extraction algorithm " + algorithm
             exit(1)
-    return returnValue
+    return value, error
 
 '''
 Arguments:
     file: Opened file to be processed
+Returns:
+    1. Just one value read from a file
+    2. an error of 0.
 Description:
     Assumes one number in the file. If there are more just the first one is
     returned.
@@ -226,7 +261,7 @@ def extractIdentity(file):
         # Check for float
         try:
             lineStripped = float(lineStripped)
-            return lineStripped
+            return lineStripped, 0.
         except Exception, e:
             print "ERROR: Found entry which is not a float"
             exit(1)
@@ -234,12 +269,14 @@ def extractIdentity(file):
 '''
 Arguments:
     file: Opened file to be processed
+Return:
+    1. The mean value of values found in file
+    2. standard deviation derived from values in file
 Description:
     Calculates and returns the mean of data points found in the file.
 '''
 def extractMean(file):
-    sum = 0.
-    nLines = 0
+    values = []
     for line in file.readlines():
         lineStripped = line.strip()
         # Check for float
@@ -249,10 +286,9 @@ def extractMean(file):
         except Exception, e:
             continue
         # Increment number of lines for averaging
-        nLines += 1
-        sum += lineStripped
-    # Finally the actual mean.
-    return sum / float(nLines)
+        values.append(lineStripped)
+    # Finally the actual mean and standard deviation are returned
+    return np.mean(values), np.std(values)
 
 
 '''
